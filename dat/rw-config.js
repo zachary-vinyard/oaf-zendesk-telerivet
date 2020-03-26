b191111d4247aa846cf69c89ec59b49d73d0327c
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-03-26 16:41:40 +0200

    rw donfig update

--- dat/rw-config.js
+++ dat/rw-config.js
@@ -7,7 +7,8 @@ needs a config function to package custom variables saved as data_packer
 module.exports = {
     'url' : 'https://oneacrefund-rw.zendesk.com/api/v2',
     'data_packer' : data_packer,
-    'ticket_table' : 'ticket_table_name' // placeholder for now - needs update
+    'ticket_table' : 'ticket_table_name', // placeholder for now - needs update
+    'update_table' : false,
 };
 
 var data_packer = data_packer = function(account_number, call_category, phone_number){ //placeholder for now needs update when available
