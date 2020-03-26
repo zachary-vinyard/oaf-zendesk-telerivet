a6e6c58ab43d63abe17696e19d474536fbc683f3
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-03-26 12:38:20 +0200

    ticket-table update

--- dat/ke-config.js
+++ dat/ke-config.js
@@ -5,7 +5,8 @@ config file for Kenya Zendesk set up
 
 module.exports = {
     'url' : null,
-    'data_packer' : data_packer
+    'data_packer' : data_packer,
+    'ticket_table' : 'ticket_table_name' // placeholder for now - needs update
 };
 
 var data_packer = function(account_number, call_category){ //placeholder for now - needs update once available
--- dat/rw-config.js
+++ dat/rw-config.js
@@ -6,7 +6,8 @@ needs a config function to package custom variables saved as data_packer
 
 module.exports = {
     'url' : 'https://oneacrefund-rw.zendesk.com/api/v2',
-    'data_packer' : data_packer
+    'data_packer' : data_packer,
+    'ticket_table' : 'ticket_table_name' // placeholder for now - needs update
 };
 
 var data_packer = function(account_number, call_category){ //placeholder for now needs update when available
