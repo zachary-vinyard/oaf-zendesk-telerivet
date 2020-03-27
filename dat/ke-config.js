c8e4252f20bb61ba061dc7580b46134ec8adcfd9
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-03-27 11:17:01 +0200

    ticket id update

--- dat/ke-config.js
+++ dat/ke-config.js
@@ -47,7 +47,7 @@ var find_user = function(account_number){
     }
 };
 
-var table_updater = function(account_number, call_category, phone_number){
+var table_updater = function(account_number, call_category, phone_number, ticket_id){
     var ticket_table = getOrCreateDataTable(opts.ticket_table);
-    ticket_table.createRow({vars : {'account_number' : account_number, 'call_category' : call_category, 'phone_number' : phone_number}});
+    ticket_table.createRow({vars : {'account_number' : account_number, 'call_category' : call_category, 'phone_number' : phone_number, 'ticket_id' : ticket_id}});
 };
