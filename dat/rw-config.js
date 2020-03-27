73b835dc47f8c5ece9674119a6e219ed6ecf0c36
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-03-27 15:51:53 +0200

    organization update

--- dat/rw-config.js
+++ dat/rw-config.js
@@ -4,13 +4,6 @@ config file for Rwanda Zendesk set up
 needs a config function to package custom variables saved as data_packer
 */
 
-module.exports = {
-    'url' : 'https://oneacrefund-rw.zendesk.com/api/v2',
-    'data_packer' : data_packer,
-    'ticket_table' : 'ticket_table_name', // placeholder for now - needs update
-    'update_table' : false,
-};
-
 var data_packer = data_packer = function(account_number, call_category, phone_number){ //placeholder for now needs update when available
     return {
         'ticket' : {
@@ -18,4 +11,11 @@ var data_packer = data_packer = function(account_number, call_category, phone_nu
             'call_category': call_category
         }
     };
+};
+
+module.exports = {
+    'url' : 'https://oneacrefund-rw.zendesk.com/api/v2',
+    'data_packer' : data_packer,
+    'ticket_table' : 'ticket_table_name', // placeholder for now - needs update
+    'update_table' : false,
 };
