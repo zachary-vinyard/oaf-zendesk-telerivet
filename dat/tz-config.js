1fc356f33f3ed6191433eba01ad2d4a05e62deee
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-05-08 12:46:31 +0200

    added tz

---
+++ dat/tz-config.js
@@ -0,0 +1,23 @@
+/*
+config file for Tanzania Zendesk set up
+**Store API key as project variable in Telerivet. do no store here.**
+needs a config function to package custom variables saved as data_packer
+*/
+
+var data_packer = data_packer = function(account_number, call_category, phone_number){ //very basic - update as needed
+    return {
+        'ticket' : {
+            'subject' : call_category,
+            'raw_subject' : call_category,
+            'status' : 'new',
+            'description' : 'USSD request for call back\nAccount number : ' + account_number,
+        }
+    };
+};
+
+module.exports = {
+    'url' : 'TZ_URL_GOES_HERE',
+    'data_packer' : data_packer,
+    'ticket_table' : 'ticket_table_name', // placeholder for now - needs update
+    'update_table' : false,
+};
