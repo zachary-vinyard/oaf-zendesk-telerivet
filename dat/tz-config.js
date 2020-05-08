c892cb298f469486aff37352cd5ac0dfc3163d8b
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-05-08 13:43:37 +0200

    json fix

--- dat/tz-config.js
+++ dat/tz-config.js
@@ -5,14 +5,14 @@ needs a config function to package custom variables saved as data_packer
 */
 
 var data_packer = data_packer = function(account_number, call_category, phone_number){ //very basic - update as needed
-    return {
+    return JSON.stringify({
         'ticket' : {
             'subject' : call_category,
             'raw_subject' : call_category,
             'status' : 'new',
             'description' : 'USSD request for call back\nAccount number : ' + account_number,
         }
-    };
+    });
 };
 
 module.exports = {
