4e2b01dc2c806557cb712fab0f897adf751a21ce
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2020-05-18 10:51:59 +0200

    tz ticket contr upd

--- dat/tz-config.js
+++ dat/tz-config.js
@@ -11,6 +11,11 @@ var data_packer = data_packer = function(account_number, call_category, phone_nu
             'raw_subject' : call_category,
             'status' : 'new',
             'description' : 'USSD request for call back\nAccount number : ' + account_number,
+            'priority' : 'normal',
+            'custom_fields' : [
+                {'id' : 360002486860, 'value' : phone_number},
+                {'id' : 360002630840, 'value' : 'ussd__please_call_me'}, //hardcoded for now but you can switch this up
+            ]
         }
     });
 };
